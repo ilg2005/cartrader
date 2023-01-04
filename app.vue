0b07f9ef7c0dd005e8ade3d73711939124aea216
ilg2005 <igoldfarb10@gmail.com>
2023-01-04 20:49:59 +0300

    Navbar

--- app.vue
+++ app.vue
@@ -1,4 +1,22 @@
 <template>
   <div>
+    <!--    NAVBAR BEGIN -->
+    <header class="
+      sticky
+      top-0
+      z-50
+      flex
+      justify-between
+      items-center
+      space-x-1
+      border-b
+      bg-white
+      p-4
+      shadow-md
+"
+    >
+      <a class="text-3xl font-mono" href="#">CarTrader</a>
+    </header>
+    <!--    NAVBAR END -->
   </div>
 </template>
