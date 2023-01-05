7ed3d1553831aa4366d834d686b1076bb08cf96d
ilg2005 <igoldfarb10@gmail.com>
2023-01-05 12:43:23 +0300

    card

--- app.vue
+++ app.vue
@@ -27,12 +27,12 @@
           <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Location</h3>
             <h3 class="text-blue-400 capitalize">Toronto</h3>
-<!--
-            <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-              <input type="text" class="border p-1 rounded">
-              <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
-            </div>
--->
+            <!--
+                        <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
+                          <input type="text" class="border p-1 rounded">
+                          <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
+                        </div>
+            -->
           </div>
 
           <div class="p-5 flex justify-between relative cursor-pointer border-b">
@@ -49,7 +49,29 @@
         <!--      CAR SIDE BAR END   -->
 
         <!--      CAR CARDS BEGIN   -->
+        <div class="w-full">
+          <!--  CAR CARD BEGIN -->
+          <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+            <div class="flex h-full">
+              <img
+                  src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg"
+                  alt=""
+                  class="w-[300px] h-full"
+              >
+              <div class="p-4 flex flex-col">
+                <div>
+                  <h1 class="text-2xl text-blue-700">
+                    Volvo XC40
+                  </h1>
+                  <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <h1 class="mt-auto text-xl">$39,555</h1>
+              </div>
+            </div>
 
+          </div>
+          <!--  CAR CARD END -->
+        </div>
         <!--      CAR CARDS END   -->
       </div>
 
