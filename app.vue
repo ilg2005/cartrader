6ded6a2d68f598cc9f720758943439a06bb9a9bb
ilg2005 <igoldfarb10@gmail.com>
2023-01-05 11:32:25 +0300

    Search Bar

--- app.vue
+++ app.vue
@@ -15,11 +15,11 @@
       shadow-md
 "
     >
-      <a class="text-3xl font-mono" href="#">CarTrader</a>
+      <a class="font-mono text-3xl" href="#">CarTrader</a>
     </header>
     <!--    NAVBAR END -->
 
-<!--    HOME HERO START  -->
+    <!--    HOME HERO START  -->
     <div class="
       relative
       h-screen
@@ -30,13 +30,42 @@
       bg-[url('https://carconfigurator.ferrari.com/assets/cars/portofinom/packages/default/car-ferrari-portofino-m_splash.jpg')]
     "
     >
-      <div class="z-10 absolute w-full h-full flex justify-center items-center">
-        <div class="text-center w-1/2">
-          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+      <div class="absolute z-10 flex h-full w-full items-center justify-center">
+        <div class="w-1/2 text-center">
+          <h1 class="mb-10 font-serif text-8xl text-white">Find your drive</h1>
+          <!--          SEARCH BAR START   -->
+          <div
+              class="
+                font-serif
+                w-[1000px]
+                text-2xl
+                rounded-full
+                bg-white
+                flex
+                justify-between
+                overflow-hidden
+                drop-shadow-2xl
+                mx-auto
+              "
+          >
+            <input type="text"
+                   class="
+                    py-3
+                    px-5
+                    w-full
+                    text-2xl
+                    rounded-full
+                    focus:outline-none
+                   "
+                   placeholder="Search by city..."
+            >
+            <button class="bg-sky-500 px-10 text-white">Search</button>
+          </div>
+          <!--          SEARCH BAR END   -->
         </div>
       </div>
-      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+      <div class="absolute h-full w-full bg-black opacity-50 z-1"></div>
     </div>
-<!--    HOME HERO END  -->
+    <!--    HOME HERO END  -->
   </div>
 </template>
