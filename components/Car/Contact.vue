55a5caf8c63405db4debeaed5ec28cae30f0c5cd
ilg2005 <igoldfarb10@gmail.com>
2023-01-06 12:49:34 +0300

    Car components + адаптивность

---
+++ components/Car/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mt-10">
+    <div class="flex flex-col space-x-0 space-y-2 lg:flex-row lg:space-y-0 w-full justify-between">
+      <input class="border p-1" placeholder="Name" type="text">
+      <input class="border p-1" placeholder="Email" type="email">
+      <input class="border p-1" placeholder="Phone" type="text">
+    </div>
+    <div class="flex mt-4 w-full">
+      <textarea class="border p-1 w-full" placeholder="Message"></textarea>
+    </div>
+    <button class="bg-blue-400 text-white px-10 py-3 rounded mt-4">Submit</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Contact"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/Car/Description.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet eligendi expedita fuga id magni
+      molestiae nobis quae quo tempora? Culpa dolore earum explicabo hic incidunt nesciunt numquam obcaecati quod
+      ut.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Description"
+}
+</script>
+
+<style scoped>
+
+</style>
