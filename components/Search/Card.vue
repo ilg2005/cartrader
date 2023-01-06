76eb2ca12d9dc158d51a892dab597780a04e304e
ilg2005 <igoldfarb10@gmail.com>
2023-01-06 11:49:39 +0300

    Search components

---
+++ components/Search/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+    <div class="flex h-full">
+      <img
+          alt=""
+          class="h-1/2 sm:h-full"
+          src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg"
+      >
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">
+            Volvo XC40
+          </h1>
+          <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+        <h1 class="mt-auto text-xl">$39,555</h1>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Card"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,55 +1,11 @@
 <template>
   <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 lg:px-16 pb-16 w-full">
     <div class="mt-32 flex">
-      <!--      CAR SIDE BAR BEGIN   -->
-      <div class="shadow border w-64 mr-10 z-30 h-1/2">
-        <div class="p-5 flex justify-between relative cursor-pointer border-b">
-          <h3>Location</h3>
-          <h3 class="text-blue-400 capitalize">Toronto</h3>
-          <!--
-                      <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-                        <input type="text" class="border p-1 rounded">
-                        <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
-                      </div>
-          -->
-        </div>
-
-        <div class="p-5 flex justify-between relative cursor-pointer border-b">
-          <h3>Make</h3>
-          <h3 class="text-blue-400 capitalize">Toyota</h3>
-        </div>
-
-        <div class="p-5 flex justify-between relative cursor-pointer border-b">
-          <h3>Price</h3>
-          <h3 class="text-blue-400 capitalize">Any</h3>
-        </div>
-
-      </div>
-      <!--      CAR SIDE BAR END   -->
+      <SearchSideBar />
 
       <!--      CAR CARDS BEGIN   -->
       <div class="w-full">
-        <!--  CAR CARD BEGIN -->
-        <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
-          <div class="flex h-full">
-            <img
-                alt=""
-                class="h-1/2 sm:h-full"
-                src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg"
-            >
-            <div class="p-4 flex flex-col">
-              <div>
-                <h1 class="text-2xl text-blue-700">
-                  Volvo XC40
-                </h1>
-                <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </div>
-              <h1 class="mt-auto text-xl">$39,555</h1>
-            </div>
-          </div>
-
-        </div>
-        <!--  CAR CARD END -->
+        <SearchCard />
       </div>
       <!--      CAR CARDS END   -->
     </div>
