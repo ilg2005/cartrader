7beea363fde95dcdab9f042e3b79be9f28306fe8
ilg2005 <igoldfarb10@gmail.com>
2023-01-25 20:09:59 +0300

    dynamic error statusCode & message

--- error.vue
+++ error.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="flex h-screen justify-center items-center flex-col">
-    <h1 class="text-9xl">404</h1>
+    <h1 class="text-9xl">{{ error.statusCode }}</h1>
     <p class="mt-7 text-4xl">
-      Page not found
+      {{ error.message }}
     </p>
-    <NuxtLink to="/" class="mt-10 rounded-xl py-2 px-4 text-2xl bg-emerald-200 shadow-xl cursor-pointer border border-emerald-500">Go Back</NuxtLink>
+    <NuxtLink class="mt-10 rounded-xl py-2 px-4 text-2xl bg-emerald-200 shadow-xl cursor-pointer border border-emerald-500"
+              to="/">
+      Go Back
+    </NuxtLink>
   </div>
 </template>
 
-<script>
-export default {
-  name: "error"
-}
+<script setup>
+
+const error = useError();
+
 </script>
 
 <style scoped>
