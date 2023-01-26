b78a61c8589d60e66b4343cd91c5a9de0df3d194
ilg2005 <igoldfarb10@gmail.com>
2023-01-26 20:17:55 +0300

    throw custom error

--- error.vue
+++ error.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex h-screen justify-center items-center flex-col">
-    <h1 class="text-9xl">{{ error.statusCode }}</h1>
+    <h1 class="text-9xl text-orange-700">{{ error.statusCode }}</h1>
     <p class="mt-7 text-4xl">
       {{ error.message }}
     </p>
@@ -16,9 +16,7 @@
 const error = useError();
 
 const handleError = () => {
-  clearError({
-    redirect: "/",
-  });
+  clearError({redirect: "/"});
 };
 
 </script>
