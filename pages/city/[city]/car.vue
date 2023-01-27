4985516f76ade9f712d5744597eb8e5718bf71d2
ilg2005 <igoldfarb10@gmail.com>
2023-01-27 12:00:29 +0300

    NuxtErrorBoundary to handle client-side errors

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,16 +1,29 @@
 <template>
 
   <div class="mt-32 flex">
-    <SearchSideBar/>
+    <NuxtErrorBoundary>
+      <SearchSideBar/>
 
-    <!--      CAR CARDS BEGIN   -->
-    <div class="flex flex-col">
-      <Card v-for="car in cars"
-            :key="car.id"
-            :product="car"
-      />
-    </div>
-    <!--      CAR CARDS END   -->
+      <!--      CAR CARDS BEGIN   -->
+      <div class="flex flex-col">
+        <Card v-for="car in cars"
+              :key="car.id"
+              :product="car"
+        />
+      </div>
+      <!--      CAR CARDS END   -->
+
+      <template #error="{error}">
+        <div class="text-center mx-auto flex flex-col">
+          <h1 class="text-red-600 text-5xl mb-4">Sorry, something went wrong...</h1>
+          <code>{{ error }}</code>
+          <button class="text-white bg-blue-400 px-10 py-3 rounded mt-8"
+                  @click="error.value = null">
+            Go Back
+          </button>
+        </div>
+      </template>
+    </NuxtErrorBoundary>
   </div>
 
 
