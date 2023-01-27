e0d7392770edfbc05a732d2c1f6fd517c2963092
ilg2005 <igoldfarb10@gmail.com>
2023-01-27 20:51:07 +0300

    useState()

--- components/Search/Card.vue
+++ components/Search/Card.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="flex shadow border w-full overflow-hidden mb-5 cursor-pointer "
+  <div class="flex shadow border w-full overflow-hidden mb-5 cursor-pointer z-0"
        @click="navigateTo(`/car/${product.name}-${product.id}`)"
   >
-
     <nuxt-img
         :src="product.url"
         alt=""
         class="h-full mr-1 md:mr-8 w-1/2"
     />
 
-    <div class="px-4 pt-2 flex flex-col">
-      <div class="flex flex-col sm:flex-row sm:justify-between">
-        <h1 class="text-xl lg:text-2xl text-blue-700 mb-2 mr-1">
+    <div class="px-4 pt-2 flex flex-col ">
+      <div class="flex justify-between ">
+        <h1 class="text-xl lg:text-2xl text-blue-700 mb-2 mr-10">
           {{ product.name }}</h1>
-        <p class="text-xl">${{ product.price }}</p>
+        <img class="w-6 h-6 z-20 mb-2" :src="isFavorite ? heartFilled : heartOutline" alt="isFavorite"
+             @click.stop="toggleFavorite"
+        />
       </div>
+      <p class="text-xl font-semibold">${{ product.price }}</p>
       <p class="hidden md:block text-gray-700">{{ product.description }}</p>
     </div>
 
@@ -24,7 +26,16 @@
 </template>
 
 <script setup>
+import heartFilled from "@/assets/heartFilled.png";
+import heartOutline from "@/assets/heartOutline.png";
+
 const car = defineProps(['product']);
+const isFavorite = useState(`favored-${car.product.id}`, () => false);
+
+const toggleFavorite = () => {
+  isFavorite.value = !isFavorite.value;
+}
+
 </script>
 
 <style scoped>
