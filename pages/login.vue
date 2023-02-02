dda08bd81b8411e66643b920838c74ba7c90ca57
ilg2005 <igoldfarb10@gmail.com>
2023-02-02 13:13:19 +0300

    текущее состояние

--- pages/login.vue
+++ pages/login.vue
@@ -1,48 +1,25 @@
-<template>
-  <div class="mt-10">
-    <p class="text-xl">User: {{ user }} </p>
-
-    <AuthCard class="mb-7"/>
-
-    <NuxtLink
-        class="p-3 text-xl hover:text-sky-900 mt-7 cursor-pointer underline transition-all ease-in-out hover:text-2xl"
-        @click="login('github')">
-      Login with Github
-    </NuxtLink>
-    <NuxtLink
-        class="p-3 text-xl hover:text-sky-900 mt-7 cursor-pointer underline transition-all ease-in-out hover:text-2xl"
-        @click="login('discord')">
-      Login with Discord
-    </NuxtLink>
-  </div>
-</template>
-
-<script lang="ts" setup>
-
-definePageMeta({
-  layout: 'custom'
-});
+<script setup lang="ts">
 const user = useSupabaseUser();
-const client = useSupabaseClient();
-const router = useRouter();
+const { auth } = useSupabaseAuthClient();
 
-const login = async (provider: 'github' | 'discord') => {
-  try {
-    const {error} = await client.auth.signInWithOAuth({provider});
-    if (error) {
-      throw error;
-      alert('Something went wrong !!');
-    } else if (user.value) {
-      router.push('/profile/listings');
-    }
-  } catch (error) {
-    alert(error.error_description || error.message);
+watchEffect(() => {
+  if (user.value) {
+    navigateTo('/profile/listings/index')
   }
-}
-
-
+})
 </script>
 
-<style scoped>
-
-</style>
+<template>
+  <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <h2 class="my-6 text-center text-3xl font-extrabold u-text-white">
+      Sign in to your account
+    </h2>
+    <AuthProviderCard>
+      <button
+          class="flex justify-center text-xl mt-3 mdi-github bg-white px-10 py-2 rounded-lg border border-gray-300 hover:bg-gray-100 font-bold"
+          @click="auth.signInWithOAuth({ provider: 'github' })"
+      ><span></span>
+        Github</button>
+    </AuthProviderCard>
+  </div>
+</template>
