2ef5ed3dd6816fa9afed8d7cf3e8a8efec83770d
ilg2005 <igoldfarb10@gmail.com>
2023-02-04 11:31:01 +0300

    cars filtered by city and query params

--- server/api/cars/[city].js
+++ server/api/cars/[city].js
@@ -2,6 +2,7 @@ import cars from "@/data/cars.json";
 export default defineEventHandler((evt) => {
     const {city} = evt.context.params;
     const {make, minPrice, maxPrice} = getQuery(evt);
+
     let filteredCars = cars.filter((car) => car.city.toLowerCase() === city.toLowerCase());
     if (make) {
         filteredCars = filteredCars.filter((car) => car.make.toLowerCase() === make.toLowerCase());
