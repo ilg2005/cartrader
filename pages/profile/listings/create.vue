5d5360589c0647446fd6718357ae77a256b71bd2
Igor <ilg2005@mail.ru>
2023-02-11 19:11:50 +0300

    текущее состояние

--- pages/profile/listings/create.vue
+++ pages/profile/listings/create.vue
@@ -4,13 +4,14 @@
       <h1 class="text-6xl">Create a New Listing</h1>
     </div>
 
-     <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
+    <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
       <ListingAdSelect
           title="Make *"
           :options="makes"
           name="make"
           @changeInput="onChangeInput"
       />
+
       <ListingAdInput
           v-for="input in inputs"
           :key="input.id"
@@ -19,15 +20,23 @@
           :placeholder="input.placeholder"
           @changeInput="onChangeInput"
       />
-       <ListingAdTextarea
-           title="Description *"
-           name="description"
-           placeholder=""
-           @changeInput="onChangeInput"
 
-       />
-       <ListingAdImage @changeInput="onChangeInput"/>
+      <ListingAdTextarea
+          title="Description *"
+          name="description"
+          placeholder=""
+          @changeInput="onChangeInput"
+
+      />
+
+      <ListingAdImage @changeInput="onChangeInput"/>
     </div>
+
+    <button @click="submitFormHandler"
+            class="shadow rounded-md p-3 mt-5 w-full bg-green-600 disabled:opacity-50 text-xl text-white enabled:hover:bg-green-800"
+            :disabled="!isValid"
+    >Submit
+    </button>
   </div>
 </template>
 
@@ -74,29 +83,49 @@ const inputs = [
   },
   {
     id: 3,
+    title: "Price *",
+    name: "price",
+    placeholder: "10000"
+  },
+
+  {
+    id: 4,
     title: "Miles *",
     name: "miles",
     placeholder: "1000"
   },
+
   {
-    id: 4,
+    id: 5,
     title: "City *",
     name: "city",
     placeholder: "Detroit"
   },
   {
-    id: 5,
+    id: 6,
     title: "Seats *",
     name: "seats",
     placeholder: "5"
   },
   {
-    id: 6,
+    id: 7,
     title: "Features *",
     name: "features",
     placeholder: "Leather Interior, No Accidents"
   },
 ];
+
+const isValid = computed(() => {
+  for (let key in info.value) {
+    if (!info.value[key]) {
+      return false;
+    }
+  }
+  return true;
+});
+const submitFormHandler = () => {
+  console.log(info.value);
+}
 </script>
 
 <style scoped>
