442928500d831023adb7a49cbc8ebe83a42af3d8
ilg2005 <igoldfarb10@gmail.com>
2023-02-18 11:52:33 +0300

    AuthForm

--- components/Auth/Form.vue
+++ components/Auth/Form.vue
@@ -23,19 +23,19 @@ const fields = reactive({
 <template>
   <div class="sm:mx-auto sm:w-full sm:max-w-md">
     <form class="shadow rounded-md overflow-hidden flex flex-col px-8 pt-8 mb-4 bg-black">
-      <label class="text-sky-500 mb-1 text-sm" for="">Email</label>
+      <label class="text-white mb-1 text-sm" for="">Email</label>
       <input v-model="fields.email"
              class="p-2 border w-100 rounded-md mb-4" name="email"
              placeholder="Enter your email"
              type="email"
       >
-      <label class="text-sky-500 mb-1 text-sm" for="">Password</label>
+      <label class="text-white mb-1 text-sm" for="">Password</label>
       <input v-model="fields.password"
              class="p-2 border w-100 rounded-md  mb-4" name="password"
              placeholder="Enter password"
              type="password"
       >
-      <button class="bg-sky-700 p-3 rounded-md text-white mb-4 uppercase"
+      <button class="bg-sky-700 hover:bg-sky-600 p-3 rounded-md text-white my-4 uppercase"
               type="submit">Submit
       </button>
       <p @click="toggleAuthState"
